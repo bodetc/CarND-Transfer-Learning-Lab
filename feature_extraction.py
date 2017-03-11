006b39503c2fba0dea0d5bd464a519f87948f7a8
Cédric Bodet <bodetc@gmail.com>
2017-03-11 12:55:14 +0100

    Tested transfer learing

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,13 +1,18 @@
 import pickle
 import tensorflow as tf
 # TODO: import Keras layers you need here
+import numpy as np
+from keras.layers import Input, Flatten, Dense
+from keras.models import Model
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
 # command line flags
-flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
-flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
+flags.DEFINE_string('training_file', 'resnet_traffic_100_bottleneck_features_train.p', "Bottleneck features training file (.p)")
+flags.DEFINE_string('validation_file', 'resnet_traffic_bottleneck_features_validation.p', "Bottleneck features validation file (.p)")
+flags.DEFINE_string('epochs', 50, "Bottleneck features validation file (.p)")
+flags.DEFINE_string('batch_size', 512, "Bottleneck features validation file (.p)")
 
 
 def load_bottleneck_data(training_file, validation_file):
@@ -21,9 +26,9 @@ def load_bottleneck_data(training_file, validation_file):
     print("Training file", training_file)
     print("Validation file", validation_file)
 
-    with open(training_file, 'rb') as f:
+    with open('./bottlenecks/'+training_file, 'rb') as f:
         train_data = pickle.load(f)
-    with open(validation_file, 'rb') as f:
+    with open('./bottlenecks/'+validation_file, 'rb') as f:
         validation_data = pickle.load(f)
 
     X_train = train_data['features']
@@ -42,12 +47,18 @@ def main(_):
     print(X_val.shape, y_val.shape)
 
     # TODO: define your model and hyperparams here
-    # make sure to adjust the number of classes based on
-    # the dataset
-    # 10 for cifar10
-    # 43 for traffic
+    nb_classes = len(np.unique(y_train))
+
+    # define model
+    input_shape = X_train.shape[1:]
+    inp = Input(shape=input_shape)
+    x = Flatten()(inp)
+    x = Dense(nb_classes, activation='softmax')(x)
+    model = Model(inp, x)
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
     # TODO: train your model here
+    model.fit(X_train, y_train, nb_epoch=FLAGS.epochs, batch_size=FLAGS.batch_size, validation_data=(X_val, y_val), shuffle=True)
 
 
 # parses flags and calls the `main` function above
